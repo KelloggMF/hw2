5c40320ceaf0e9f5c51c1c64761c8fa19ead99c5
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-05-02 16:24:48 +0000

    Began generating Studio model

--- kmdb.rb
+++ kmdb.rb
@@ -68,11 +68,10 @@
 # The Dark Knight Rises  Anne Hathaway         Selina Kyle
 
 # Delete existing data, so you'll start fresh each time this script is run.
-# Use `Model.destroy_all` code.
-# TODO!
+Studio.destroy_all
 
 # Generate models and tables, according to the domain model.
-# TODO!
+rails generate model Studio
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
