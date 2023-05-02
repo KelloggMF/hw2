5236e971abef4f4ff653ee22c96f16ca0641b73c
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-05-02 19:14:37 +0000

    More table building

--- kmdb.rb
+++ kmdb.rb
@@ -122,7 +122,47 @@ batman3 = Movie.find_by({ "title" => "The Dark Knight Rises" })
 
 new_actor = Actor.new
 new_actor["name"] = "Christian Bale"
-new_studio.save
+new_actor.save
+
+new_actor2 = Actor.new
+new_actor2["name"] = "Michael Caine"
+new_actor2.save
+
+new_actor3 = Actor.new
+new_actor3["name"] = "Liam Neeson"
+new_actor3.save
+
+new_actor4 = Actor.new
+new_actor4["name"] = "Katie Holmes"
+new_actor4.save
+
+new_actor5 = Actor.new
+new_actor5["name"] = "Gary Oldman"
+new_actor5.save
+
+new_actor6 = Actor.new
+new_actor6["name"] = "Heath Ledger"
+new_actor6.save
+
+new_actor7 = Actor.new
+new_actor7["name"] = "Aaron Eckhart"
+new_actor7.save
+
+new_actor8 = Actor.new
+new_actor8["name"] = "Maggie Gyllenhaal"
+new_actor8.save
+
+new_actor9 = Actor.new
+new_actor9["name"] = "Tom Hardy"
+new_actor9.save
+
+new_actor10 = Actor.new
+new_actor10["name"] = "Joseph Gordon-Levitt"
+new_actor10.save
+
+new_actor11 = Actor.new
+new_actor11["name"] = "Anne Hathaway"
+new_actor11.save
 
 # Prints a header for the movies output
 puts "Movies"
